1ccb1a16e830bd0ee66c327d08bbde7a4c648c96
Firestar311 <firestar311mc@gmail.com>
2018-05-11 03:18:38 -0500

    Remove the setrank command which has been moved to a new command on FirecraftCore, will eventually be moved here.

--- src/java/net/firecraftmc/socket/server/Main.java
+++ src/java/net/firecraftmc/socket/server/Main.java
@@ -5,7 +5,8 @@ import net.firecraftmc.shared.classes.*;
 import net.firecraftmc.shared.enums.Rank;
 import net.firecraftmc.shared.packets.FPacketRankUpdate;
 import org.bukkit.ChatColor;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -138,67 +139,7 @@ public class Main extends JavaPlugin implements Listener {
     }
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("setrank")) {
-            if (sender instanceof ConsoleCommandSender) {
-                sender.sendMessage("§cIt is not currently implemented for console to set ranks.");
-            } else if (sender instanceof Player) {
-                FirecraftPlayer player = this.localPlayers.get(((Player) sender).getUniqueId());
-                if (!(player.getMainRank().equals(Rank.FIRECRAFT_TEAM) || player.getMainRank().equals(Rank.HEAD_ADMIN))) {
-                    player.sendMessage("&cYou are not allowed to set ranks.");
-                    return true;
-                }
-                
-                if (!(args.length == 2)) {
-                    player.sendMessage("&cYou do not have enough arguments.");
-                    return true;
-                }
-                String targetName = args[0];
-                
-                UUID uuid;
-                try {
-                    uuid = Utils.Mojang.getUUIDFromName(targetName);
-                } catch (Exception e) {
-                    player.sendMessage("&cThere was an error getting the uuid from the name you provided.");
-                    return true;
-                }
-                
-                if (uuid == null) {
-                    player.sendMessage("&cThe name you provided could not be resolved to a unique id.");
-                    return true;
-                }
-                
-                FirecraftPlayer target = Utils.getPlayerFromDatabase(server, database, uuid);
-                
-                if (target == null) {
-                    player.sendMessage("&cThere was no player found for the name: " + targetName);
-                    return true;
-                }
-                
-                String baseTR = args[1].toUpperCase();
-                Rank targetRank;
-                try {
-                    targetRank = Rank.valueOf(baseTR);
-                } catch (IllegalArgumentException e) {
-                    player.sendMessage("&cThe rank name provided was incorrect.");
-                    return true;
-                }
-                
-                if (targetRank.equals(Rank.FIRECRAFT_TEAM)) {
-                    player.sendMessage("&cThe Firecraft Team rank cannot be assigned through a command.");
-                    return true;
-                }
-                
-                if (target.getMainRank().isEqualToOrHigher(player.getMainRank())) {
-                    player.sendMessage("&cYou cannot assign the rank of someone of your current rank or higher.");
-                    return true;
-                }
-                
-                database.updateSQL("UPDATE `playerdata` SET `mainrank`='" + target.getMainRank().toString() + "' WHERE `uniqueid`='" + uuid.toString().replace("-", "") + "';");
-                String prefix = (!targetRank.equals(Rank.PRIVATE)) ? targetRank.getPrefix() : targetRank.getBaseColor() + "Private";
-                player.sendMessage("&aSuccessfully set §e" + target.getName() + "&a's rank to " + prefix);
-                SocketWorker.sendToAll(new FPacketRankUpdate(server, player.getUniqueId(), target.getUniqueId()));
-            }
-        } else if (cmd.getName().equalsIgnoreCase("createprofile")) {
+        if (cmd.getName().equalsIgnoreCase("createprofile")) {
             if (sender instanceof Player) {
                 FirecraftPlayer player = this.localPlayers.get(((Player) sender).getUniqueId());
                 if (!player.getMainRank().equals(Rank.FIRECRAFT_TEAM)) {
